74fcf2f302744385243e4080037bc4cececb1294
dawarland <dawarland@gmail.com>
2020-05-24 16:13:32 +0200

    test deploy 1

---
+++ build/js/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    changeColor();
+    $('li>a').on('click', function(e) {
+        e.preventDefault();
+        var hash = this.hash;
+        $('html, body').animate({
+            scrollTop: $(this.hash).offset().top
+        }, 1000, function(){
+            window.location.hash = hash;
+        });
+    });
+    $('.navbar-nav').on('click', function (e) {
+        //$('.navbar-toggler').click(); //bootstrap 3.x
+        console.log(e);
+    });
+});
+function changeColor(){
+    if ( location.hash == '#project'){
+        if ( document.getElementById("sideNav").classList.contains('bg-primary') ){
+            document.getElementById("sideNav").classList.remove('bg-primary');
+            document.getElementById("sideNav").classList.add('bg-dark');
+        }
+    }else {
+        if ( document.getElementById("sideNav").classList.contains('bg-dark') ){
+            document.getElementById("sideNav").classList.remove('bg-dark');
+            document.getElementById("sideNav").classList.add('bg-primary');
+        }
+    }
+}
